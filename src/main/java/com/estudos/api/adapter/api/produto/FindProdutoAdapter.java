46f9f71c69ae5f917f9bcd2337caaf1771a623a5
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-08-02 17:37:43 -0300

    commit antes de sair

--- src/main/java/com/estudos/api/adapter/api/produto/FindProdutoAdapter.java
+++ src/main/java/com/estudos/api/adapter/api/produto/FindProdutoAdapter.java
@@ -1,5 +1,22 @@
 package com.estudos.api.adapter.api.produto;
 
-public class FindProdutoAdapter {
+import java.util.Collection;
+
+import com.estudos.api.adapter.api.model.ProdutoDto;
+import com.estudos.api.application.ports.persistence.entrypoint.produto.FindProdutoPort;
+
+public class FindProdutoAdapter implements FindProdutoPort{
+
+	@Override
+	public Collection<ProdutoDto> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProdutoDto fetchById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
